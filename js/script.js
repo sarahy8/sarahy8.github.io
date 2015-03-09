58b8c0c333c2b995fa64ddb933039abbf89047f1
Sarah <sarahyeo2@gmail.com>
2015-03-08 17:06:06 -0700

    My site

---
+++ js/script.js
@@ -0,0 +1,100 @@
+/*!
+ * Start Bootstrap - Grayscale Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery to collapse the navbar on scroll
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+
+var container = document.querySelector('.isotope');
+var msnry = new Masonry( container, {
+  // options
+  columnWidth: 345,
+  itemSelector: '.isotope-item',
+  isFitWidth: true
+});
+
+
+// jQuery
+/*
+var $container = $('.isotope');
+// init
+$container.isotope({
+  // options
+  itemSelector: '.isotope-item',
+  //animationEngine : 'best-available',
+  masonry: {
+  columnWidth: 305,
+  //isFitWidth: true
+},
+
+});
+
+
+// filter items on button click
+$('#filters').on( 'click', 'button', function() {
+  var filterValue = $(this).attr('data-filter');
+  $container.isotope({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.is-checked').removeClass('is-checked');
+      $( this ).addClass('is-checked');
+    });
+  });
+
+*/
+
+
+$(document).ready(function () {
+
+    $('.projectpopup').popup({
+        pagecontainer: '.container',
+        transition: 'all 0.3s',
+        //scrolllock: true
+    });
+
+});
+
+
+var div = $('.intro-text').hide(),
+    news = ['Software developer.', 'Computer science student.'],
+    count = 0;
+
+function changeNews() {
+    div.fadeIn(1000).delay(4000).fadeOut(800, function() {
+        changeNews();
+    }).delay(200).text(news[count++])
+    if (count == news.length) {
+        count = 0;
+    }
+}
+
+changeNews();
