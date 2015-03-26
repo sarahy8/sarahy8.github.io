f20f6c6df87a0430abbf3e4ab38aaec02ac79ab9
Sarah <sarahyeo2@gmail.com>
2015-03-26 01:45:22 -0700

    New intro page

--- js/script.js
+++ js/script.js
@@ -29,7 +29,7 @@ $(function() {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1200, 'easeInOutExpo');
         event.preventDefault();
     });
 });
@@ -108,4 +108,23 @@ function changeNews() {
 }
 
 changeNews();
+
+
+//’secret’ specifies the numerical keystrokes that make up the word “mario”
+var secret = "8365826572"; 
+var input = "";
+var timer;
+//The following function sets a timer that checks for user input. You can change the variation in how long the user has to input by changing the number in ‘setTimeout.’ In this case, it’s set for 500 milliseconds or ½ second.
+$(document).keyup(function(e) {
+   input += e.which;    
+   clearTimeout(timer);
+   timer = setTimeout(function() { input = ""; }, 500);
+   check_input();
+});
+//Once the time is up, this function is run to see if the user’s input is the same as the secret code
+function check_input() {
+    if(input == secret) {
+        //the code used to reveal mario and the world is then put here           
+    }
+};
 */
