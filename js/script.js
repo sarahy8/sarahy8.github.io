594d11497a15b89bb4200a9fd616f144ce6e666a
Sarah <sarahyeo2@gmail.com>
2015-03-31 01:53:58 -0700

    Changing text

--- js/script.js
+++ js/script.js
@@ -98,9 +98,9 @@ $('.intro').css({'background-image': 'url(img/' + images[Math.floor(Math.random(
 
 
 
-/*
+
 var div = $('.intro-text').hide(),
-    news = ['Software developer.', 'Computer science student.'],
+    news = ['Software developer and student.', 'Based out of Vancouver, BC.'],
     count = 0;
 
 function changeNews() {
@@ -113,26 +113,6 @@ function changeNews() {
 }
 
 changeNews();
-
-
-//’secret’ specifies the numerical keystrokes that make up the word “mario”
-var secret = "8365826572"; 
-var input = "";
-var timer;
-//The following function sets a timer that checks for user input. You can change the variation in how long the user has to input by changing the number in ‘setTimeout.’ In this case, it’s set for 500 milliseconds or ½ second.
-$(document).keyup(function(e) {
-   input += e.which;    
-   clearTimeout(timer);
-   timer = setTimeout(function() { input = ""; }, 500);
-   check_input();
-});
-//Once the time is up, this function is run to see if the user’s input is the same as the secret code
-function check_input() {
-    if(input == secret) {
-        //the code used to reveal mario and the world is then put here           
-    }
-};
-*/
 
 $(window).load(function() {
   $(".loader").fadeOut("slow");
